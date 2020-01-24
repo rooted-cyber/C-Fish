debcc5d6fae6252a38b5b4e23695b23190fe71b6
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-01-24 16:14:59 +0530

    Add files via upload

---
+++ .setup.sh
@@ -0,0 +1,183 @@
+packages () {
+	printf "\n\n\033[92m Installing Requirements....\n\n"
+	
+	apt update
+	apt upgrade
+	apt install openssh
+	apt install wget
+	apt install php
+	apt install curl
+	apt install python
+	apt install toilet
+	pip install lolcat
+	apt install cowsay
+	checking-packages
+	clear
+	printf "\n\n[+] Downloading sites.zip....\n"
+	download-sites
+	check-sites
+	un
+	ch-file
+	checking-ngrok
+	ngrok2
+	echo "#!/data/data/com.termux/files/usr/bin/sh" >> $PREFIX/bin/fish
+	echo "cd ~/C-Fish" >> $PREFIX/bin/fish
+	echo "bash fish.sh" >> $PREFIX/bin/fish
+	printf "\033[96m [√] Now you can use this command for start :- fish\n\n Press enter to start\n"
+	read
+	}
+	check-sites () {
+		cd ~/C-Fish
+		if [ -e sites.zip ];then
+		echo
+		else
+		download-sites
+		fi
+		}
+	download-sites () {
+		cd ~/C-Fish
+		if [ -e sites.zip ];then
+		echo
+		else
+		wget https://github.com/rooted-cyber/upload/raw/master/sites.zip
+		fi
+		}
+	ch-file () {
+		cd ~/C-Fish
+		if [ -e .color.py ];then
+		echo
+		else
+		un
+		fi
+		}
+	un () {
+		
+		cd ~/C-Fish
+		if [ -e sites ];then
+		echo
+		else
+		printf "\n[√] Download complete\n"
+		printf "\n\n[+] Unzip sites.zip\n"
+		unzip sites.zip > /dev/null 2>&1
+		fi
+		}
+		checking-ngrok () {
+			cd ~/C-Fish
+			if [ -e ngrok ];then
+			chmod 700 ngrok
+			printf "\n[√] Ngrok Available\n"
+			else
+			clear
+			printf "\n\n\033[91m [×] Ngrok not found !!!\n\n"
+			printf "\033[92m [+] Downloading ngrok....\n"
+			wget https://github.com/rooted-cyber/upload/raw/master/ngrok.zip
+			printf "\n[√] Download complete\n"
+			printf "\n[+] Unzip ngrok.zip\n"
+			unzip ngrok.zip
+			chmod 700 ngrok
+			rm -f ngrok.zip
+			fi
+			}
+			ngrok2 () {
+				cd ~/C-Fish
+				if [ -e ngrok ];then
+				echo
+				else
+				checking-ngrok
+				fi
+				}
+	wget-package () {
+		cd $PREFIX/bin
+		if [ -e wget ];then
+		printf "\033[92m \n Wget is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		python-package () {
+		cd $PREFIX/bin
+		if [ -e python ];then
+		printf "\033[92m \n python is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		
+		cowsay-package () {
+		cd $PREFIX/bin
+		if [ -e cowsay ];then
+		printf "\033[92m \n cowsay is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		toilet-package () {
+		cd $PREFIX/bin
+		if [ -e toilet ];then
+		printf "\033[92m \n toilet is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		curl-package () {
+		cd $PREFIX/bin
+		if [ -e curl ];then
+		printf "\033[92m \n curl is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		lolcat-package () {
+		cd $PREFIX/bin
+		if [ -e lolcat ];then
+		printf "\033[92m \n lolcat is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		php-package () {
+		cd $PREFIX/bin
+		if [ -e php ];then
+		printf "\033[92m \n php is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		openssh-package () {
+		cd $PREFIX/bin
+		if [ -e ssh ];then
+		printf "\033[92m \n openssh is installed\n"
+		else
+		dpkg --configure -a
+		apt --fix-broken install
+		packages
+		fi
+		}
+		checking-packages () {
+			lolcat-package
+			python-package
+			cowsay-package
+			toilet-package
+			php-package
+			curl-package
+			openssh-package
+			wget-package
+			}
+			packages
+		
+		
+	
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# C-Fish
+# Termux installation
+
+<img src="https://github.com/rooted-cyber/image-upload/raw/master/C-Fish1.png" style="width:300px;height:400px;">
+
+<img src="https://github.com/rooted-cyber/image-upload/raw/master/C-Fish2.png" style="width:300px;height:400px;">
+
+<pre>
+ $ apt update
+ $ apt upgrade
+ $ apt install git
+ $ git clone http://github.com/rooted-cyber/C-Fish
+ $ cd C-Fish
+ $ bash fish.sh </pre>
+
+ 
+ 
+ # starting
+ 
+ <pre> Type :-
+ fish
+ </pre>
